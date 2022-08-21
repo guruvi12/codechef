3f2cf6729e700f4017d1839b6d748af2e88ea80b
guruvi12 <73025383+guruvi12@users.noreply.github.com>
2022-08-21 15:51:27 +0530

    Update code.cpp

--- Air_conditioner_temperature/code.cpp
+++ Air_conditioner_temperature/code.cpp
@@ -1,6 +1,20 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a,b,c;
+
+	cin>>t;
+    for(int i=0;i<t;i++){
+	cin>>a>>b>>c;
+    if(a<=b && c<=b){
+       cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }}
+    return 0;
+	
 }
+
+
